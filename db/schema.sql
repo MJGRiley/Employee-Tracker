b16ef4a1b69f9e5c03d61212d7517d916c2196e1
MJGR <mjgriley@gmail.com>
2021-10-17 17:47:40 -0400

    getting some things setup, ready for the next part

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE  role (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  dept_id INT NOT NULL
+  FOREIGN KEY (dept_id)
+  REFERENCES department(id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (dept_name)
+VALUES (Engineering),
+       (Finance),
+       (Legal),
+       (Sales);
+
+INSERT INTO role (title, salary)
+VALUES ('Sales Lead', 100000),
+       ('SalesPerson', 80000),
+       ('Lead Engineer', 150000),
+       ('Software Engineer', 120000),
+       ('Account Manager', 160000),
+       ('Accountant', 125000),
+       ('Legal Team Lead', 250000),
+       ('Lawyer', 190000);
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ('John', 'Doe', 'manager_id'),
+       ('Mike','Chan', 'manager_id'),
+       ('Ashley','Rodriguez', 'manager_id'),
+       ('Kevin','Tupik', 'manager_id'),
+       ('Kunal','Singh', 'manager_id'),
+       ('Malia','Brown', 'manager_id'),
+       ('Sarah','Lourd', 'manager_id'),
+       ('Tom','Allen', 'manager_id');
